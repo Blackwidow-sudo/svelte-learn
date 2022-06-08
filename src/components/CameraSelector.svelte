fe4f09c56fc2d7bb5c9c04d86c587335c6593f11
Blackwidow-sudo <bob_merlin92@web.de>
2022-06-08 10:59:48 +0200

    add CameraSelector

---
+++ src/components/CameraSelector.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { CamAbbr } from 'src/types';
+
+    export let availableCameras: CamAbbr[];
+</script>
+
+<select name="camera" id="camera" on:change>
+    <option value="none" disabled selected>Select a Camera</option>
+    {#each availableCameras as camera}
+        <option value={camera.toLowerCase()}>{camera}</option>
+    {/each}
+</select>
+
+<style lang="scss">
+</style>
