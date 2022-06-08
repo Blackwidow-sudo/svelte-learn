7aa3e89562b832fdb9c67e5c53556588b58b160b
Blackwidow-sudo <bob_merlin92@web.de>
2022-06-08 20:35:26 +0200

    update RoverSelect

--- src/components/RoverSelect.svelte
+++ src/components/RoverSelect.svelte
@@ -6,8 +6,8 @@
 
 <select name="selectRover" id="selectRover" on:change>
     <option value="none" disabled selected>Select a Rover</option>
-    {#each availableRovers as item}
-        <option value={item.toLowerCase()}>{item}</option>
+    {#each availableRovers as rover}
+        <option value={rover.toLowerCase()}>{rover}</option>
     {/each}
 </select>
 
