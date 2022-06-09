31da880165327eb1f514a89f774ad2bc1854640f
Blackwidow-sudo <bob_merlin92@web.de>
2022-06-09 11:18:10 +0200

    add TypeGuards isCamAbbr & isArrayOfCameras

--- src/lib/typeGuards.ts
+++ src/lib/typeGuards.ts
@@ -1,4 +1,5 @@
-import type { DateString, Photo, RoverManifest } from '../types';
+import type { CamAbbr, DateString, Photo, RoverManifest } from '../types';
+import { cameraDescriptions } from '../globals';
 
 function isManifest(mani: unknown): mani is RoverManifest {
     return (mani as RoverManifest).launch_date !== undefined;
@@ -6,6 +7,15 @@ function isManifest(mani: unknown): mani is RoverManifest {
 
 function isPhoto(photo: unknown): photo is Photo {
     return (photo as Photo).img_src !== undefined;
+}
+
+function isCamAbbr(str: unknown): str is CamAbbr {
+    if (typeof str !== 'string') {
+        return false;
+    }
+    if (cameraDescriptions.hasOwnProperty(str.toUpperCase())) {
+        return true;
+    } else return false;
 }
 
 function isArrOfPhotos(arr: unknown): arr is Photo[] {
@@ -18,8 +28,20 @@ function isArrOfPhotos(arr: unknown): arr is Photo[] {
     return isPhoto(arr[0]);
 }
 
+function isArrayOfCameras(arr: unknown): arr is CamAbbr[] {
+    if (!Array.isArray(arr)) {
+        return false;
+    }
+    arr.forEach((str) => {
+        if (!isCamAbbr(str)) {
+            return false;
+        }
+    });
+    return true;
+}
+
 function isDateString(date: string): date is DateString {
     return isNaN(Date.parse(date));
 }
 
-export { isManifest, isPhoto, isArrOfPhotos, isDateString };
+export { isManifest, isPhoto, isCamAbbr, isArrOfPhotos, isArrayOfCameras, isDateString };
