25d84cba521f99d054ac422a0a6f0e9454ed5c26
Blackwidow-sudo <bob_merlin92@web.de>
2022-06-27 11:45:16 +0200

    add new components for image lazy loading

---
+++ src/components/lazyload/Image.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    export let src: string;
+    export let alt: string;
+
+    let loaded = false;
+    let thisImage: HTMLImageElement;
+
+    onMount(() => {
+        thisImage.onload = () => {
+            loaded = true;
+        };
+    });
+</script>
+
+<img {src} {alt} class:loaded bind:this={thisImage} referrerpolicy="no-referrer" />
+
+<style>
+    img {
+        width: 100%;
+        min-height: 700px;
+        opacity: 0;
+        transition: opacity 1200ms ease-out;
+    }
+
+    img.loaded {
+        opacity: 1;
+    }
+</style>
---
+++ src/components/lazyload/ImageLoader.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import IntersectionObserver from './IntersectionObserver.svelte';
+    import Image from './Image.svelte';
+
+    export let src;
+    export let alt;
+</script>
+
+<IntersectionObserver once={true} let:intersecting>
+    {#if intersecting}
+        <Image {src} {alt} />
+    {/if}
+</IntersectionObserver>
